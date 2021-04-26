00305af4c409469967a24081772192b2846bbb79
pgadgul <81529839+pgadgul@users.noreply.github.com>
2021-04-26 02:42:48 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,19 +1,28 @@
+# Import required libraries
+import os
+from random import randint
+
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+import flask
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
 
-import flask
-import pandas as pd
-import time
-import os
 
-app = dash.Dash()
+# Setup the app
+# Make sure not to change this file name or the variable names below,
+# the template is configured to execute 'server' on 'app.py'
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
 
-app.layout = html.Div([
-            html.H1(children="Hello world!",className="hello",
-    style={'color':'#00361c','text-align':'center'
-          })
-      ])
-if __name__=='__main__':
-    app.run_server()
+
+# Put your Dash code here
+
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.server.run(debug=True, threaded=True)
