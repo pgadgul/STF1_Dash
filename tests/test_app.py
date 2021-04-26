1bcc5d800a99631823d199434ff47d06ccd15aa8
pgadgul <81529839+pgadgul@users.noreply.github.com>
2021-04-26 01:00:59 -0400

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,12 @@
-from app import index
+def test_ok():
+    assert 0 == 0 # Test is ok
 
-def test_index():
-    assert index()== "Hello,world!"
+
+def test_failed():
+    #assert 1 == 0 # Test failed REMOVE THIS PART AND USE assert not
+    assert not 1 == 0
+
+
+
+def test_different_exit_code():
+    open('/nonexistent', 'r')
