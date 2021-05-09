ebda7ffce656db7baed175e09fda6b8651054f9c
pgadgul <poojagadgul@gmail.com>
2021-05-09 13:16:50 -0400

    Modified data and dash app

--- src/app.py
+++ src/app.py
@@ -4,14 +4,17 @@ from random import randint
 
 import plotly.plotly as py
 from plotly.graph_objs import *
-
+import pandas as pd
+import numpy as np
 import flask
 import dash
 
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
-
+# Import data
+data = pd.read_csv("../data/2019-01-29_SEN_Hk_tlm_t.csv")
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
@@ -21,11 +24,63 @@ app = dash.Dash(__name__, server=server)
 
 
 # Put your Dash code here
-app.layout = html.Div([
-            html.H1(children="Hello world!",className="hello",
-    style={'color':'#00361c','text-align':'center'
-          })
-      ])
+app.layout = html.Div(
+    children=[
+        html.H1(children="STF1 Data Analysis",),
+        html.P(
+            children="Analyze the behavior of STF1 sensor reading while orbiting around earth"
+            " There are Temperature,Gyro and Magnetometer readings available ",
+        ),
+        dcc.Dropdown(
+            id='demo-dropdown',
+            options=[
+                {'label': 'Temprature X axis', 'value': 'TEMP_0'},
+                {'label': 'Temprature Y axis', 'value': 'TEMP_1'},
+                {'label': 'Magnetometer X axis', 'value': 'MAG_0'},
+                {'label': 'Magnetometer Y axis', 'value': 'MAG_1'},
+                {'label': 'Magnetometer Z axis', 'value': 'MAG_2'},
+                {'label': 'Gyrometer X axis', 'value': 'GYRO_0'},
+                {'label': 'Gyrometer Y axis', 'value': 'GYRO_1'},
+                {'label': 'Gyrometer Z axis', 'value': 'GYRO_2'},
+            ],
+            value='TEMP0'
+        ),
+        html.Div(id='dd-output-container'),
+        dcc.Graph(
+
+            figure={
+                "data": [
+                    {
+
+                        "x": data["CCSDS_SECONDS"],
+                        "y": data["MAG_0"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Magnetic field analysis"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["CCSDS_SECONDS"],
+                        "y": data["GYRO_0"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Gyrometer Data Analysis"},
+            },
+        ),
+    ]
+)
+
+# Add  callback
+@app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('demo-dropdown', 'value')])
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
 
 # Run the Dash app
 if __name__ == '__main__':
