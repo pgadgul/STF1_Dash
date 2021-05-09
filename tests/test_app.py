b81d4b4276948b6cddbc7a063678318d7f06bb0b
pgadgul <poojagadgul@gmail.com>
2021-05-09 13:29:55 -0400

    Modified test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,37 +1,30 @@
+# 1. imports of your dash app
 import dash
-from dash.dependencies import Output, Input
-from dash.exceptions import PreventUpdate
-
 import dash_html_components as html
 
-from pytest_dash import wait_for
 
-def test_application(dash_threaded):
-    # The selenium driver is available on the fixture.
-    driver = dash_threaded.driver
+# 2. give each testcase a tcid, and pass the fixture
+# as a function argument, less boilerplate
+def test_bsly001_falsy_child(dash_duo):
+
+    # 3. define your app inside the test function
     app = dash.Dash(__name__)
-    counts = {'clicks': 0}
+    app.layout = html.Div(id="nully-wrapper", children=0)
 
-    app.layout = html.Div([
-        html.Div('My test layout', id='out'),
-        html.Button('click me', id='click-me')
-    ])
+    # 4. host the app locally in a thread, all dash server configs could be
+    # passed after the first app argument
+    dash_duo.start_server(app)
 
-    @app.callback(
-        Output('out', 'children'),
-        [Input('click-me', 'n_clicks')]
-    )
-    def on_click(n_clicks):
-        if n_clicks is None:
-            raise PreventUpdate
+    # 5. use wait_for_* if your target element is the result of a callback,
+    # keep in mind even the initial rendering can trigger callbacks
+    dash_duo.wait_for_text_to_equal("#nully-wrapper", "0", timeout=4)
 
-        counts['clicks'] += 1
-        return 'Clicked: {}'.format(n_clicks)
+    # 6. use this form if its present is expected at the action point
+    assert dash_duo.find_element("#nully-wrapper").text == "0"
 
-    dash_threaded(app)
+    # 7. to make the checkpoint more readable, you can describe the
+    # acceptance criterion as an assert message after the comma.
+    assert dash_duo.get_logs() == [], "browser console should contain no error"
 
-    btn = wait_for.wait_for_element_by_css_selector(driver, '#click-me')
-    btn.click()
-
-    wait_for.wait_for_text_to_equal(driver, '#out', 'Clicked: 1')
-    assert counts['clicks'] == 1
+    # 8. visual testing with percy snapshot
+    dash_duo.percy_snapshot("bsly001-layout")
