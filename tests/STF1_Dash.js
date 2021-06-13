c7c36779286aa9dd3d48f9198b13dcaee50225a8
pgadgul <poojagadgul@gmail.com>
2021-06-12 22:13:03 -0400

    add testcafe sample test

--- tests/STF1_Dash.js
+++ tests/STF1_Dash.js
@@ -1,15 +1,16 @@
-import { Selector } from 'testcafe'; // first import testcafe selectors
+import { Selector } from 'testcafe';
 
-fixture `Getting Started`// declare the fixture
-    .page `https://devexpress.github.io/testcafe/example`;  // specify the start page
+fixture `New Fixture`
+    .page `http://127.0.0.1:8050/`;
 
-
-//then create a test and place your code there
-test('My first test', async t => {
+test('New Test', async t => {
     await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button')
-
-        // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+        .click(Selector('#react-entry-point h1').withText('STF1 Data Analysis'))
+        .expect(Selector('#react-entry-point h1').withText('STF1 Data Analysis').textContent).eql('STF1 Data Analysis')
+        .click(Selector('#react-select-2--value div').withText('Temprature X axis'))
+        .click('#demo-dropdown .Select-arrow-zone')
+        .click('#demo-dropdown .Select-arrow')
+        .click(Selector('#react-select-2--list div').withText('Magnetometer X axis').nth(3))
+        .expect(Selector('#dd-output-container div').withText('1.5M').nth(3).id).eql("")
+        .click(Selector('#dd-output-container div').withText('1.5M').nth(3));
 });
