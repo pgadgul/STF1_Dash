f8929059fc2190215acf0984fd76f46e91abc6a7
pgadgul <poojagadgul@gmail.com>
2021-06-13 02:37:59 -0400

    modified pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,4 @@
-from selenium.webdriver.chrome.options import Options
 from dash.testing.application_runners import import_app
-
-def pytest_setup_options():
-    """called before webdriver is initialized"""
-    options = Options()
-    options.add_argument('--disable-gpu')
-    return options
 
 
 def test_one(dash_duo):
