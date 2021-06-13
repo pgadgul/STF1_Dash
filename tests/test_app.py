8f6ee43cdda05c03c34b58bfed8d33b0ef7fdf43
pgadgul <poojagadgul@gmail.com>
2021-06-13 19:47:54 -0400

    Updated pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+import dash
+import dash_html_components as html
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
@@ -5,3 +7,25 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('http://example.com')
+
+def test_bsly001_falsy_child(dash_duo):
+
+    # 3. define your app inside the test function
+    app = dash.Dash(__name__)
+    app.layout = html.Div(id="nully-wrapper", children=0)
+
+    # 4. host the app locally in a thread, all dash server configs could be
+    # passed after the first app argument
+    dash_duo.start_server(app)
+
+    # 5. use wait_for_* if your target element is the result of a callback,
+    # keep in mind even the initial rendering can trigger callbacks
+    dash_duo.wait_for_text_to_equal("#nully-wrapper", "0", timeout=4)
+
+    # 6. use this form if its present is expected at the action point
+    assert dash_duo.find_element("#nully-wrapper").text == "0"
+
+    # 7. to make the checkpoint more readable, you can describe the
+    # acceptance criterion as an assert message after the comma.
+    assert dash_duo.get_logs() == [], "browser console should contain no error"
+
