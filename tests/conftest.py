46f49ad4594d52f37e3ca4b0aa6af32109d0f674
pgadgul <poojagadgul@gmail.com>
2021-06-13 09:08:37 -0400

    modified pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
-# add this in the conftest.py under tests folder
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import chromedriver_binary  # Adds chromedriver binary to path
 
-def pytest_setup_options():
-    options = Options()
-    options.add_argument('--disable-gpu')
-    return options
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
